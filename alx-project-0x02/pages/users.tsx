39550d129e99c5865366041de974783a4f96fd77
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-05 18:00:23 +0300

    8 Build a UserCard Component

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -26,6 +26,7 @@ const Users = ({ users }: UsersPageProps) => {
   )
 }
 
+// ✅ Add getStaticProps to statically fetch users
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
   const data = await res.json()
